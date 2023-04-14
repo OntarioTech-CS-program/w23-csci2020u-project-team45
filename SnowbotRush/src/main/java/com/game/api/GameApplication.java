d37bc3541a2165c3922d63f6f9a8b0cfa8202605
sara-bhoira <122528434+sara-bhoira@users.noreply.github.com>
2023-04-14 18:03:59 -0400

    project causing maven issues

--- SnowbotRush/src/main/java/com/game/api/GameApplication.java
+++
@@ -1,7 +0,0 @@
-package com.game.api;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api") // this is a general api application call path
-public class GameApplication extends Application {}
